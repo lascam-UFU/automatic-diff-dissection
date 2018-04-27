11a77a2131bc0d0d021c3af32bf6b41eb2eb4365
tdurieux <durieuxthomas@hotmail.com>
2018-04-27 09:55:48 +0200

    fix: decode proprely the content of the file

--- src/main/java/fr/inria/spirals/main/Utils.java
+++ src/main/java/fr/inria/spirals/main/Utils.java
@@ -1,8 +1,12 @@
 package fr.inria.spirals.main;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,9 +27,12 @@ public class Utils {
 
     public static List<String> fileToLines(String filename) {
         List<String> lines = new LinkedList<String>();
-        String line = "";
+        String line;
         try {
-            BufferedReader in = new BufferedReader(new FileReader(filename));
+            InputStream is = new FileInputStream(filename);
+            int BUFFER_SIZE = 8192;
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(is, Charset.forName("ISO-8859-1")), BUFFER_SIZE);
             while ((line = in.readLine()) != null) {
                 lines.add(line);
             }
