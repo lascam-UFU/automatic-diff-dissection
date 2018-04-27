6fab72a4649c022920cde6a1ceabdc9d6405baab
tdurieux <durieuxthomas@hotmail.com>
2018-04-27 11:51:29 +0200

    be able to execute the main script outside of the project

--- scripts/defects4j/run-tool-on-defects4j/Launch.py
+++ scripts/defects4j/run-tool-on-defects4j/Launch.py
@@ -11,8 +11,9 @@ output_path = config.get('path', 'output')
 
 def get_project_features(project, bug_id, mode):
     jar = os.path.join(root, "target", "patchclustering-0.1-SNAPSHOT-jar-with-dependencies.jar")
-    cmd = """java -jar %s --bugId %s --buggySourceDirectory %s --diff %s -m %s""" % \
-          (jar,
+    cmd = """cd %s;java -jar %s --bugId %s --buggySourceDirectory %s --diff %s -m %s""" % \
+          (root,
+           jar,
            bug_id,
            os.path.join(defects4j_checkout_path, project, bug_id, "buggy-version"),
            os.path.join(defects4j_checkout_path, project, bug_id, "path.diff"),
