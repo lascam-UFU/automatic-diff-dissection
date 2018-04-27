ded802f9f4951fb40142ba5dca78a5bc8b1cdb2d
tdurieux <durieuxthomas@hotmail.com>
2018-04-27 10:02:54 +0200

    remove unused import

--- src/main/java/fr/inria/spirals/main/Utils.java
+++ src/main/java/fr/inria/spirals/main/Utils.java
@@ -2,7 +2,6 @@ package fr.inria.spirals.main;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
