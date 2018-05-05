9db9d3fa06065ebe4b8330f3c219c56bceb9ff8d
tdurieux <durieuxthomas@hotmail.com>
2018-05-05 16:22:43 +0200

    fix wrong variable

--- scripts/defects4j/compare-manual-and-automatic-results/Compare.py
+++ scripts/defects4j/compare-manual-and-automatic-results/Compare.py
@@ -18,7 +18,7 @@ def find_info_from_bug(bug_id, manual_analysis_info):
 
 def compare_feature(bug_info1, bug_info2, feature, feature_type):
     automatic_info = bug_info1[feature_type][feature]
-    manual_info = feature in bug_info2[feature]
+    manual_info = feature in bug_info2[feature_type]
     if automatic_info > 0 and manual_info:
         return "Both detected"
     if automatic_info == 0 and not manual_info:
