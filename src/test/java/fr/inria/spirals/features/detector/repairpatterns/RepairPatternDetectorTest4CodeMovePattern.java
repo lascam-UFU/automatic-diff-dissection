3569a04606bb3b427b6fa4c0d1731a5b06d47a0b
tdurieux <durieuxthomas@hotmail.com>
2018-05-09 16:00:03 +0200

    add code move detector test

---
+++ src/test/java/fr/inria/spirals/features/detector/repairpatterns/RepairPatternDetectorTest4CodeMovePattern.java
@@ -0,0 +1,86 @@
+package fr.inria.spirals.features.detector.repairpatterns;
+
+import fr.inria.spirals.entities.RepairPatterns;
+import fr.inria.spirals.main.Config;
+import fr.inria.spirals.utils.TestUtils;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by tdurieux
+ *
+ */
+public class RepairPatternDetectorTest4CodeMovePattern {
+
+    @Test
+    public void closure13() {
+        Config config = TestUtils.setupConfig("Closure 13");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+
+    @Test
+    public void closure68() {
+        Config config = TestUtils.setupConfig("Closure 68");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+
+    @Test
+    public void closure102() {
+        Config config = TestUtils.setupConfig("Closure 102");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+
+    @Test
+    public void closure107() {
+        Config config = TestUtils.setupConfig("Closure 117");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+
+    @Test
+    public void lang53() {
+        Config config = TestUtils.setupConfig("Lang 53");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+
+    @Test
+    public void math64() {
+        Config config = TestUtils.setupConfig("Math 64");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+
+    @Test
+    @Ignore
+    public void math86() {
+        Config config = TestUtils.setupConfig("Math 86");
+
+        RepairPatternDetector detector = new RepairPatternDetector(config);
+        RepairPatterns repairPatterns = detector.analyze();
+
+        Assert.assertTrue(repairPatterns.getFeatureCounter("codeMove") > 0);
+    }
+}
