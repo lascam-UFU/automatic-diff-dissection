eb5ca74fa6fef85a1998bd9533ae1644935b7488
Martin Monperrus <martin.monperrus@gnieh.org>
2020-04-03 10:29:00 +0200

    @monperrus at work

--- src/main/java/add/features/detector/spoon/SpoonHelper.java
+++ src/main/java/add/features/detector/spoon/SpoonHelper.java
@@ -32,8 +32,7 @@ public class SpoonHelper {
             String fileContent = String.join(Constants.LINE_BREAK, files.get(path))
                     .replace("import javax.annotation.Nullable", "// import javax.annotation.Nullable")
                     .replace("import javax.annotation.CheckForNull", "// import javax.annotation.CheckForNull");
-            VirtualFile virtualFile = new VirtualFile(fileContent, new File(path).getName());
-            spoon.getModelBuilder().addInputSource(virtualFile);
+            spoon.getModelBuilder().addInputSource(new File(path));
         }
         spoon.buildModel();
 
